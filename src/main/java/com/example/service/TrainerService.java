807a3e0dd950f5c940188181b833733d43f001bc
paulollito29-sketch <paulollito29@gmail.com>
2026-02-06 14:43:12 -0500

    add comment over method findAll

--- src/main/java/com/example/service/TrainerService.java
+++ src/main/java/com/example/service/TrainerService.java
@@ -14,38 +14,39 @@ public class TrainerService {
 
     private final TrainerRepository trainerRepository;
 
-    public TrainerService(TrainerRepository trainerRepository){
+    public TrainerService(TrainerRepository trainerRepository) {
         this.trainerRepository = trainerRepository;
     }
 
-    public List<TrainerFindAll> findAll(){
+    //method findAll obtains trainers
+    public List<TrainerFindAll> findAll() {
         return trainerRepository.findAllByEnabledIsTrueOrderByIdTrainerDesc().stream()
                 .map(TrainerMapper::ToTrainerFindAll)
                 .toList();
     }
 
-    public TrainerCreated create(TrainerCreate dto){
-        if(trainerRepository.existsByEnabledIsTrueAndNameIgnoreCase(dto.name())){
-            throw new ResourceAlreadyExistsException("this name: "+dto.name()+" already exists");
+    public TrainerCreated create(TrainerCreate dto) {
+        if (trainerRepository.existsByEnabledIsTrueAndNameIgnoreCase(dto.name())) {
+            throw new ResourceAlreadyExistsException("this name: " + dto.name() + " already exists");
         }
         var trainerEntity = TrainerMapper.ToEntity(dto);
         var trainerSaved = trainerRepository.save(trainerEntity);
         return TrainerMapper.ToTrainerCreated(trainerSaved);
     }
 
-    public TrainerFindOne findOne(Long id){
+    public TrainerFindOne findOne(Long id) {
         var trainer = trainerRepository.findFirstByEnabledIsTrueAndIdTrainer(id)
-                .orElseThrow(()-> new ResourceNotFoundException("Trainer not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Trainer not found"));
 
         return TrainerMapper.ToTrainerFindOne(trainer);
     }
 
-    public TrainerUpdated update(Long id, TrainerUpdate dto){
+    public TrainerUpdated update(Long id, TrainerUpdate dto) {
         var trainer = trainerRepository.findFirstByEnabledIsTrueAndIdTrainer(id)
-                .orElseThrow(()-> new ResourceNotFoundException("Trainer not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Trainer not found"));
 
-        if(trainerRepository.existsByEnabledIsTrueAndNameIgnoreCase(dto.name())){
-            throw new ResourceAlreadyExistsException("this name: "+dto.name()+" already exists");
+        if (trainerRepository.existsByEnabledIsTrueAndNameIgnoreCase(dto.name())) {
+            throw new ResourceAlreadyExistsException("this name: " + dto.name() + " already exists");
         }
 
         var trainerUpdate = TrainerMapper.toEntity(trainer, dto);
@@ -53,9 +54,9 @@ public class TrainerService {
         return TrainerMapper.toTrainerUpdated(trainerSaved);
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         var trainer = trainerRepository.findFirstByEnabledIsTrueAndIdTrainer(id)
-                .orElseThrow(()-> new ResourceNotFoundException("Trainer not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Trainer not found"));
         var trainerDeleted = TrainerMapper.toEntity(trainer);
         trainerRepository.save(trainerDeleted);
     }
