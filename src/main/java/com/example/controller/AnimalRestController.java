326c734b921f0ad182a346b302b72b89ea4e9246
paulollito29-sketch <paulollito29@gmail.com>
2026-02-16 12:14:52 -0500

    new comment

--- src/main/java/com/example/controller/AnimalRestController.java
+++ src/main/java/com/example/controller/AnimalRestController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
-//new commentasdas
+//new
+//
 @RestController
 @RequestMapping("/api/animals")
 public class AnimalRestController {
