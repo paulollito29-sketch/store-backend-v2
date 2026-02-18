945f9f3d34a942d168919e20a84eec679f01bf18
paulollito29-sketch <paulollito29@gmail.com>
2026-02-17 23:59:13 -0500

    service speciality

--- src/main/java/com/example/mapper/SpecialityMapper.java
+++ src/main/java/com/example/mapper/SpecialityMapper.java
@@ -1,7 +1,7 @@
 package com.example.mapper;
 
-import com.example.dto.SpecialityCreate;
-import com.example.dto.SpecialityFindAll;
+import com.example.dto.*;
+import com.example.entity.FacultyEntity;
 import com.example.entity.SpecialityEntity;
 
 import java.time.LocalDateTime;
@@ -14,11 +14,36 @@ public class SpecialityMapper {
         return new SpecialityFindAll(entity.getIdSpeciality(), entity.getFaculty().getName(), entity.getName());
     }
 
-    public static SpecialityEntity toEntityCreated(SpecialityEntity entity, SpecialityCreate dto){
-        entity.setCreatedAt(LocalDateTime.now());
+    public static SpecialityEntity toEntityCreated(SpecialityCreate dto, FacultyEntity faculty){
+        return SpecialityEntity.builder()
+                .name(dto.name())
+                .faculty(faculty)
+                .enabled(true)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public static SpecialityCreated toSpecialityCreated(SpecialityEntity entity){
+        return new SpecialityCreated(entity.getIdSpeciality(), entity.getFaculty().getIdFaculty(), entity.getName());
+    }
+
+    public static SpecialityFindOne toFindOne(SpecialityEntity entity){
+        return new SpecialityFindOne(entity.getIdSpeciality(), entity.getFaculty().getIdFaculty(), entity.getName());
+    }
+    public static SpecialityEntity toEntityUpdated(SpecialityEntity entity, SpecialityUpdate dto, FacultyEntity faculty){
         entity.setName(dto.name());
-        entity.setFaculty();
+        entity.setFaculty(faculty);
+        entity.setUpdatedAt(LocalDateTime.now());
+        return entity;
+    }
 
+    public static SpecialityUpdated toSpecialityUpdated(SpecialityEntity entity){
+        return new SpecialityUpdated(entity.getIdSpeciality(), entity.getFaculty().getIdFaculty(), entity.getName());
+    }
 
+    public static SpecialityEntity toEntityDeleted(SpecialityEntity entity){
+        entity.setEnabled(false);
+        entity.setDeletedAt(LocalDateTime.now());
+        return entity;
     }
 }
--- src/main/java/com/example/service/SpecialityService.java
+++ src/main/java/com/example/service/SpecialityService.java
@@ -1,6 +1,9 @@
 package com.example.service;
 
-import com.example.dto.SpecialityFindAll;
+import com.example.dto.*;
+import com.example.exception.ResourceAlreadyExistsException;
+import com.example.exception.ResourceNotFoundException;
+import com.example.mapper.SpecialityMapper;
 import com.example.repository.FacultyRepository;
 import com.example.repository.SpecialityRepository;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,56 @@ public class SpecialityService {
     public SpecialityService(FacultyRepository facultyRepository, SpecialityRepository specialityRepository){
         this.facultyRepository = facultyRepository;
         this.specialityRepository = specialityRepository;
+    }
+    public List<SpecialityFindAll> findAll(){
+        return specialityRepository.findAllByEnabledIsTrueOrderByIdSpecialityDesc()
+                .stream()
+                .map(SpecialityMapper::toFindAll)
+                .toList();
+    }
+
+    public SpecialityCreated create(SpecialityCreate dto){
+        if (specialityRepository.existsByEnabledIsTrueAndNameIgnoreCase(dto.name())){
+            throw new ResourceAlreadyExistsException("this speciality already exists");
+        }
+
+        var faculty = facultyRepository.findFirstByEnabledIsTrueAndIdFaculty(dto.facultyId())
+                .orElseThrow(() -> new ResourceNotFoundException("no faculty found"));
+
+        var speciality = SpecialityMapper.toEntityCreated(dto, faculty);
+        var specialitySaved = specialityRepository.save(speciality);
+        return SpecialityMapper.toSpecialityCreated(specialitySaved);
+    }
+
+    public SpecialityFindOne findOne(Long id){
+        var speciality = specialityRepository.findFirstByEnabledIsTrueAndIdSpeciality(id)
+                .orElseThrow(() -> new ResourceNotFoundException("no speciality found"));
+
+        return SpecialityMapper.toFindOne(speciality);
+    }
+
+    public SpecialityUpdated update(SpecialityUpdate dto, Long id){
+        var speciality = specialityRepository.findFirstByEnabledIsTrueAndIdSpeciality(id)
+                .orElseThrow(() -> new ResourceNotFoundException("no speciality found"));
+
+        if (specialityRepository.existsByEnabledIsTrueAndIdSpecialityNotAndNameIgnoreCase(id, dto.name())){
+            throw new ResourceAlreadyExistsException("this speciality already exists");
+        }
+
+        var faculty = facultyRepository.findFirstByEnabledIsTrueAndIdFaculty(dto.facultyId())
+                .orElseThrow(() -> new ResourceNotFoundException("no faculty found"));
+
+        var specialityUpdated = SpecialityMapper.toEntityUpdated(speciality, dto, faculty);
+        var specialitySaved = specialityRepository.save(specialityUpdated);
+        return SpecialityMapper.toSpecialityUpdated(specialitySaved);
+    }
+
+    public void delete(Long id){
+        var speciality = specialityRepository.findFirstByEnabledIsTrueAndIdSpeciality(id)
+                .orElseThrow(() -> new ResourceNotFoundException("no speciality found"));
+
+        var specialityDeleted = SpecialityMapper.toEntityDeleted(speciality);
+        specialityRepository.save(specialityDeleted);
     }
 
 
